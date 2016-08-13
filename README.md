e49495c0531c89e759fb76220b64617d02625563
Martin Eve <martin@martineve.com>
2016-08-13 14:39:19 +0100

    Add popularity search script

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 (c) Copyright Martin Paul Eve, 2016. Released under the GNU Public Affero License v3. See LICENSE for more. 
 
+# dateFile.py
+
 Takes a file called infile.txt and produces an output file, dated_outfile.csv, that contains a list of dates for each word in infile.txt.
 
 The program expects there to be one word per line in infile.txt.
@@ -7,3 +9,7 @@ The program expects there to be one word per line in infile.txt.
 Anticipated usage is for verifying linguistic accuracy of language used in historical fiction/parody.
 
 At the moment, this tool searches for usages on dictionary.com that specify a date range (e.g. 1900-1905) and single dates (900, "before 900") etc.
+
+# findPopularity.py
+
+Uses Merriam Webster's "how popular is this word" feature to produce an output CSV of popularity for words within "outfile.txt".
---
+++ findPopularity.py
@@ -0,0 +1,73 @@
+import csv
+import re
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    print('Beginning text popularity assessment using Merriam Webster.')
+
+    # build a regular expression pattern that can be re-used to search for popularity expressions in
+    # Merriam Webster entries
+    popularity_pattern = re.compile('(Top|Bottom) \d+% of words')
+
+    # build a headers dictionary that looks like a real computer and not a machine
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+               'Chrome/39.0.2171.95 Safari/537.36'}
+
+    with open('outfile.txt', 'r') as text:
+        with open('popularity_outfile.csv', 'w') as csv_out:
+            # create a csv-writer object
+            output_writer = csv.writer(csv_out, delimiter=',', quotechar='\'', quoting=csv.QUOTE_MINIMAL)
+
+            # read the input text line by line into the text_list variable
+            text_list = text.readlines()
+
+            # iterate over the words
+            for word in text_list:
+                # remove any trailing newline characters
+                word = word.strip('\n')
+
+                # create a string of a URL that
+                url = 'http://stats.merriam-webster.com/pop-score-redesign.php?word={0}&id=popularity-score'.format(word)
+
+                # wait 10 seconds so that we do not hammer the dictionary.com server
+                time.sleep(10)
+
+                # Get the fetched page
+                fetched_page = requests.get(url, headers=headers).text
+
+                # test whether dictionary.com has a usage section
+                has_usage = popularity_pattern.search(fetched_page)
+
+                if has_usage:
+
+                    # look through the document for a span tag where the text matches the date_pattern
+                    result = popularity_pattern.search(fetched_page)
+
+                    if result:
+                        popularity = result.group()
+
+                        # write the found date to the CSV
+                        print('[{0}]: {1}'.format(word, popularity))
+                        output_writer.writerow([word, popularity])
+                    else:
+                        write_failure(output_writer, word)
+                else:
+                    write_failure(output_writer, word)
+
+                    # write the output to the file in case we crash
+                csv_out.flush()
+
+    print('Done.')
+
+
+def write_failure(output_writer, word):
+    # write a blank row to the output CSV indicating that we found no date
+    print('[{0}]: Found no popularity'.format(word))
+    output_writer.writerow([word, 'NO POPULARITY'])
+
+if __name__ == "__main__":
+    main()
